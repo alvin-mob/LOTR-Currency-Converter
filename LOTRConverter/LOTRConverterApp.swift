6d1a3130f565ce445ccab7ab9934a411993bb080
alwined <alwin@zoftsolutions.com>
2024-03-18 12:25:02 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Abraham Cyriac on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
